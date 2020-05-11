ace790345f71414ef005699f377829d8e2a935fb
jeter888 <jeter@ucdavis.edu>
2020-05-10 17:33:45 -0700

    retrieve SONA ID & download to text file

--- prelim.js
+++ prelim.js
@@ -3,11 +3,11 @@ function WriteToFile(passForm) {
     var fso = CreateObject("Scripting.FileSystemObject");
     var s   = fso.CreateTextFile("SONA data/filename.txt", true);
 
-    var firstName = document.getElementById('FirstName');
-    var lastName  = document.getElementById('lastName');
+    var sonaID = document.getElementById('txtID');
+    // var lastName  = document.getElementById('lastName');
 
-    s.writeline("First Name :" + firstName);
-    s.writeline("Last Name :" + lastName);
+    s.writeline("First Name :" + sonaID);
+    // s.writeline("Last Name :" + lastName);
 
     s.writeline("-----------------------------");
     s.Close();
