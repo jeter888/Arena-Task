cc6239f26a2649f2b41389ee340361c6798366c5
jeter888 <jeter@ucdavis.edu>
2020-05-13 15:14:22 -0700

    Commented out file download. Vals are currently stored for later

--- prelim.js
+++ prelim.js
@@ -8,30 +8,48 @@ let appendSubjectToFile = () => {
         '\r SONA ID: ' + name.value + ' \r\n ';
 
 
-
     // Convert the text to BLOB.
-    const textToBLOB = new Blob([data], { type: 'text/plain' });
+    const textToBLOB = new Blob([data], {type: 'text/plain'});
 
     //set the filename equal to the SONA ID (can change later)
     const sFileName = name.value;	   // The file to save the data.
 
     let newLink = document.createElement("a");
-    newLink.setAttribute("src", "SONA data/");
-    newLink.download = sFileName;
-
-    if (window.webkitURL != null) {
-        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
-    }
-    else {
-        newLink.href = window.URL.createObjectURL(textToBLOB);
-        newLink.style.display = "none";
-        document.body.appendChild(newLink);
-    }
-
-    newLink.click();
-
-    //after sona data saves, redirect to experiment page
+    //#FIXME: commented this last section out to download file for later. For now, values are stored in variables
+    // newLink.download = sFileName;
+    //
+    // if (window.webkitURL != null) {
+    //     newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    // } else {
+    //     newLink.href = window.URL.createObjectURL(textToBLOB);
+    //     newLink.style.display = "none";
+    //     document.body.appendChild(newLink);
+    // }
+    //
+    // newLink.click();
     window.location.assign("index.html");
-    // drawArena();
-
 }
+
+//     const fs = require('fs');
+//
+// // specify the path to the file, and create a buffer with characters we want to write
+//     let path = 'SONA_data/';
+//     let buffer = new Buffer('Those who wish to follow me\nI welcome with my hands\nAnd the red sun sinks at last');
+//     fs.open(path, 'w', function(err, fd) {
+//         if (err) {
+//             throw 'could not open file: ' + err;
+//         }
+//         fs.write(fd, buffer, 0, buffer.length, null, function(err) {
+//             if (err) throw 'error writing file: ' + err;
+//             fs.close(fd, function() {
+//                 console.log('wrote the file successfully');
+//             });
+//         });
+//     });
+//
+//
+//     //after sona data saves, redirect to experiment page
+//     window.location.assign("index.html");
+//     // drawArena();
+//
+// }
